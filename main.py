d6123b0cdececeb4196060044ccd0a49f254e37c
ali imran <aliimranlaskar123@gmail.com>
2025-07-16 03:17:44 +0530

    Clean commit without .env

---
+++ main.py
@@ -0,0 +1,67 @@
+import discord 
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token =os.getenv('DISCORD_TOKEN')
+
+
+handler =logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content =True
+intents.members = True
+
+bot = commands.Bot(command_prefix='!',intents=intents)
+secret_role ="Tousif ar bou"
+@bot.event
+async def on_ready():
+    print(f"We are ready to go {bot.user.name}")
+@bot.event
+async def on_member_join(member):
+    await member.send(f'Welcome to the server {member.name}!')
+
+
+@bot.event
+async def on_message(message):
+    if message.author ==bot.user:
+        return
+    if "hannan" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} Respect who give you birth!")
+
+    await bot.process_commands(message)     
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello bsdk {ctx.author.name}")
+
+
+@bot.command()
+async def tousif_ar_bou(ctx):
+    role = discord.utils.get(ctx.guild.roles,name=secret_role)
+    if role:
+        await ctx.author.add_roles(role)
+        await ctx.send(f"Role {secret_role} has been assigned to {ctx.author.name}")
+
+    else:
+        await ctx.send(f"Role {secret_role} does not exist")
+
+@bot.command()
+async def tousif_talak(ctx):
+    role =discord.utils.get(ctx.guild.roles,name=secret_role)
+    if role:
+        await ctx.author.remove_roles(role)
+        await ctx.send(f"Touif ar {ctx.author.name} somporko ses ")
+    else:
+        await ctx.send(f"Role {secret_role} does not exist")    
+
+
+
+bot.run(token,log_handler=handler,log_level=logging.DEBUG)
+
+
+
+
